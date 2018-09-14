b2d8052e34e6530bcbc61357a55fadfb4ddb5067
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-09-14 13:44:43 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -3,3 +3,16 @@ import '../lib/selectize.min';
 
 //select
 $('select').selectize();
+
+//menu mob
+$('.menu-open').on('click', function() {
+  $('.menu').addClass('active');
+  $('.bg').fadeIn(1000);
+  $('body').css({'overflow': 'hidden'});
+});
+
+$('.bg, .menu-close').on('click', function() {
+  $('.menu').removeClass('active');
+  $('.bg').fadeOut();
+  $('body').css({'overflow': 'visible'});
+});
